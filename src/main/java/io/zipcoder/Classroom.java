14d986eb19c2bfb739f5b64755b5564482234fa0
Seth <seth@Zipcoders-MacBook-Pro.local>
2023-03-14 10:37:53 -0400

    tested and implemented a good portion of classroom

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,58 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Classroom {
+    Student[] students;
+
+    public Classroom(){
+        students = new Student[30];
+    }
+
+    public Classroom(int maxStudents){
+        students = new Student[maxStudents];
+    }
+
+    public Classroom(int maxStudents, Student... student){
+        students = new Student[maxStudents];
+        int counter = 0;
+        for(Student s: student){
+            students[counter] = s;
+            counter++;
+        }
+    }
+
+    public Student[] getStudentByScore(double score) { //come back and sort this
+        ArrayList<Student> s = new ArrayList<>();
+        for(Student stu: students){
+            if(stu != null) {
+                if (stu.getAverageExamScore() == score) {
+                    s.add(stu);
+                }
+            }
+        }
+        return s.toArray(new Student[0]);
+    }
+
+    public double getAverageExamScore() {
+        double sum = 0.0;
+        for(Student s : students){
+            sum+= s.getAverageExamScore();
+        }
+        return sum/students.length;
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public void addStudent(Student student) {
+        for (int i = 0; i < students.length; i++) {
+            if(students[i] == null){
+                students[i] = student;
+                return;
+            }
+        }
+        System.out.println("Error, class is full");
+    }
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,56 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ClassroomTest {
+    @Test
+    public void testGetStudentByScore() { //come back and sort this
+        Student[] expected = new Student[1];
+        expected[0] = new Student("John", "Doe", new Double[]{100.0});
+        Classroom c = new Classroom(1, expected[0]);
+
+        Student[] actual = c.getStudentByScore(100.0);
+
+        Assert.assertEquals(expected[0],actual[0]);
+    }
+
+    @Test
+    public void testGetAverageExamScore() {
+        double expected = 100.0;
+        Student s1 = new Student(null, null, new Double[]{150.0});
+        Student s2 = new Student(null, null, new Double[]{100.0});
+        Student s3 = new Student(null, null, new Double[]{50.0});
+        Classroom c = new Classroom(3, s1, s2, s3);
+
+        double actual = c.getAverageExamScore();
+
+        Assert.assertEquals(expected, actual, .02);
+    }
+
+    @Test
+    public void testGetStudents() {
+        Student s1 = new Student(null, null, new Double[]{150.0});
+        Student s2 = new Student(null, null, new Double[]{100.0});
+        Student s3 = new Student(null, null, new Double[]{50.0});
+        Classroom c = new Classroom(3, s1, s2, s3);
+
+        Student[] actual = c.getStudents();
+
+        Assert.assertEquals(actual[0], s1);
+        Assert.assertEquals(actual[1], s2);
+        Assert.assertEquals(actual[2], s3);
+    }
+
+    @Test
+    public void testAddStudent() {
+        Student s1 = new Student(null, null, new Double[]{150.0});
+        Student s2 = new Student(null, null, new Double[]{100.0});
+        Classroom c = new Classroom(3, s1);
+
+        c.addStudent(s2);
+        Student actual = c.getStudentByScore(100.0)[0];
+
+        Assert.assertEquals(s2, actual);
+    }
 }

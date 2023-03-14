d88acdc7d682794988ec0ebe8c9f236793a9e2fc
Seth <seth@Zipcoders-MBP.home>
2023-03-14 06:30:24 -0400

    tests then basic student methods written, student not complete

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,57 @@
 package io.zipcoder;
 
+import java.util.Arrays;
+
 public class Student {
+    String firstName;
+    String lastName;
+    Double[] scores;
+    public Student(String firstName, String lastName, Double[] examScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.scores = examScores;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getExamScores() {
+        StringBuilder examScores = new StringBuilder();
+        examScores.append("Exam Scores:\n");
+        for (int i = 0; i < scores.length; i++) {
+            examScores.append("\tExam ").append(i+1).append(": ").append(scores[i]).append("\n");
+        }
+        return String.valueOf(examScores);
+    }
+
+    public int getNumberOfExamsTaken(){
+        return scores.length;
+    }
+
+    public void addExamScore(double score) {
+        Double[] newScores = Arrays.copyOf(scores, scores.length +1);
+        newScores[newScores.length-1] = score;
+        scores = newScores;
+    }
+
+    public void setExamScore(int exam, double score) {
+        scores[exam-1] = score;
+    }
+
+    public Double getAverageExamScore() {
+        return null;
+    }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,93 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StudentTest {
 
+    @Test
+    public void testGetExamScores() {
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = { 100.0, 95.0, 123.0, 96.0 };
+        Student student = new Student(firstName, lastName, examScores);
+        String expected = "Exam Scores:\n" +
+                "\tExam 1: 100.0\n" +
+                "\tExam 2: 95.0\n" +
+                "\tExam 3: 123.0\n" +
+                "\tExam 4: 96.0\n";
+
+        // When
+        String output = student.getExamScores();
+
+        // Then
+        Assert.assertEquals(expected,output);
+    }
+
+    @Test
+    public void testAddExamScore() {
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = { };
+        Student student = new Student(firstName, lastName, examScores);
+        String expected = "Exam Scores:\n\tExam 1: 100.0\n";
+        // When
+        student.addExamScore(100.0);
+        String output = student.getExamScores();
+
+        // Then
+        Assert.assertEquals(expected,output);
+
+    }
+
+    @Test
+    public void testSetExamScore() {
+
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = { 100.0 };
+        Student student = new Student(firstName, lastName, examScores);
+        String expected = "Exam Scores:\n\tExam 1: 150.0\n";
+
+        // When
+        student.setExamScore(1, 150.0);
+        String output = student.getExamScores();
+
+        // Then
+        Assert.assertEquals(expected,output);
+    }
+
+    @Test
+    public void testGetAverage() {
+
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        Double output = student.getAverageExamScore();
+
+        // Then
+        System.out.println(output);
+    }
+
+    @Test
+    public void testToString() {
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        String output = student.toString();
+
+        // Then
+        System.out.println(output);
+    }
 }
